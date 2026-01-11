c71f1dfb812b99d670dbbfc615c0bd7067a317ca
Jasper Andrew <jqaxyz@gmail.com>
2026-01-11 14:46:01 -0500

    finish dialog

--- godot/interface/components/dialog.gd
+++ godot/interface/components/dialog.gd
@@ -4,6 +4,18 @@ extends CenterContainer
 @onready var btn_stay = $PanelContainer/MarginContainer/MarginContainer/MarginContainer/VBoxContainer/HBoxContainer/BtnStay
 
 func _ready() -> void:
-	GameManager.player.moved_exit.connect(show)
-	btn_descend.pressed.connect(GameManager.level.load_next_level)
-	btn_stay.pressed.connect(hide)
+	GameManager.player.moved_exit.connect(show_dialog)
+	btn_descend.pressed.connect(_on_btn_descend_pressed)
+	btn_stay.pressed.connect(hide_dialog)
+
+func show_dialog() -> void:
+	show()
+	get_tree().paused = true
+
+func hide_dialog() -> void:
+	hide()
+	get_tree().paused = false
+
+func _on_btn_descend_pressed() -> void:
+	GameManager.level.load_next_level()
+	get_tree().paused = false
