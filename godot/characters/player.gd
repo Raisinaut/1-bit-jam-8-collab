ec9f3202ff4fb4f39c71918861c4081e43a5e48f
Jasper Andrew <jqaxyz@gmail.com>
2026-01-11 22:01:16 -0500

    improve input parsing

--- godot/characters/player.gd
+++ godot/characters/player.gd
@@ -8,6 +8,11 @@ signal touched_ghost
 
 var movement_timer : SceneTreeTimer
 
+var last_x = 0
+var last_y = 0
+
+enum Axis { X, Y, NONE }
+var last_axis = Axis.NONE
 
 func _ready() -> void:
 	GameManager.player = self
@@ -15,13 +20,32 @@ func _ready() -> void:
 	moved.connect(_on_moved)
 	bonked.connect(_on_bonked)
 	ghost_detection.area_entered.connect(_on_ghost_detection_area_entered)
-	
+
 func _process(_delta: float) -> void:
-	var direction = Vector2.ZERO
-	direction.x = Input.get_axis("move_left", "move_right")
-	direction.y = Input.get_axis("move_up", "move_down")
-	if direction:
-		attempt_move(direction)
+	var x = Input.get_axis("move_left", "move_right")
+	var y = Input.get_axis("move_up", "move_down")
+	
+	var direction = get_move_vector(x,y)
+	if direction.x != 0:   last_axis = Axis.X
+	elif direction.y != 0: last_axis = Axis.Y
+	else: last_axis = Axis.NONE
+
+	last_x = x
+	last_y = y
+	
+	attempt_move(direction)
+
+func get_move_vector(x: float, y: float) -> Vector2:
+	if x != 0 and y != 0:
+		print(x," ",y)
+		if last_x == 0:   return Vector2(x, 0)
+		elif last_y == 0: return Vector2(0, y)
+		else:
+			if last_axis == Axis.X: return Vector2(x, 0)
+			if last_axis == Axis.Y: return Vector2(0, y)
+	
+	return Vector2(x,y)
+
 
 func _on_moved() -> void:
 	footsteps.play()
